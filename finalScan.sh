863013893a35d8d6f420fa6bf04594ff4078053b
estewart1 <35575682+estewart1@users.noreply.github.com>
2018-05-18 11:58:44 -0500

    Add files via upload

---
+++ finalScan.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+export KUBECONFIG=~/Downloads/dev-kubeconfig_yaml.yaml
+rawAWS=$(aws ec2 --profile fishtech describe-volumes | jq '.Volumes[] | .State, .VolumeId')
+AWS="$(echo "$rawAWS" | fgrep -xv '"in-use"')"
+final="$(echo "$AWS" | egrep -A 1 -o '"available"')"
+if [ -z "$final" ]
+then
+    echo "All in-use" && exit 0
+else
+    rawKUBE=$(kubectl describe pv | egrep -o 'vol-\w+')
+    arr=($rawKUBE)
+    ARR=()
+    for i in "${arr[@]}"
+    do 
+        ARR+="$(echo $final | egrep -o $i)"
+    done
+    if [ -z "$ARR"]
+    then
+        remove="$(echo $final | egrep -o "vol-\w+")"
+        echo "$remove should be deleted" && exit 1
+    else
+        echo 'Available volumes listed in Kubernetes' && exit 0
+    fi
+fi
